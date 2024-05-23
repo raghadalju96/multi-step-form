6e77708461f1139610ac96a5cc264db220536a11
raghadalju96 <raghad.alju96@gmail.com>
2024-05-23 15:38:29 +0300

    multi-step-form

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   const nextSteps = document.querySelectorAll(".next-step");
   const preSteps = document.querySelectorAll(".prev-step");
-  console.log(nextSteps);
+  const monthlySvg = document.getElementById("monthlySvg");
+  const yearlySvg = document.getElementById("yearlySvg");
+  const monthlyPrices = document.querySelectorAll(".monthly-price");
+  const yearlyPrices = document.querySelectorAll(".yearly-price");
+  const billingCycleContainer = document.querySelector(".billing-cycle");
+  const billingCycleLabels = document.querySelectorAll(".billing-cycle-lable");
+  const plansContainer = document.querySelector(".plans");
+  const plans = document.querySelectorAll(".plan");
 
   nextSteps.forEach((nextStep) => {
     nextStep.addEventListener("click", () => {
@@ -15,6 +22,34 @@ document.addEventListener("DOMContentLoaded", () => {
       const currentStep = parseInt(preStep.getAttribute("data-step"));
       showStep(currentStep - 1);
     });
+  });
+
+  plansContainer.addEventListener("click", (event) => {
+    const plan = event.target.closest(".plan");
+    console.log(plan);
+
+    if (!plan) return;
+
+    plans.forEach((plan) => {
+      plan.classList.remove("active");
+    });
+    plan.classList.add("active");
+  });
+
+  billingCycleContainer.addEventListener("click", (event) => {
+    if (event.target.tagName === "LABEL" || event.target.closest("LABEL")) {
+      monthlySvg.classList.toggle("d-none");
+      yearlySvg.classList.toggle("d-none");
+
+      billingCycleLabels.forEach((billingCycleLabel) => {
+        billingCycleLabel.classList.toggle("active");
+      });
+
+      // Toggle plan prices
+
+      monthlyPrices.forEach((price) => price.classList.toggle("d-none"));
+      yearlyPrices.forEach((price) => price.classList.toggle("d-none"));
+    }
   });
 });
 
